9de299fce408626caf0365a5760225c1cc4837c1
Alexandre Dias <dias.al@husky.neu.edu>
2017-03-28 14:54:48 -0400

    02:54
    Added Comments to public methods in Damage.java

--- src/damage/Damage.java
+++ src/damage/Damage.java
@@ -2,6 +2,7 @@ package damage;
 
 import monsters.Monster;
 
+import java.util.List;
 import java.util.Random;
 
 import general.Attack;
@@ -16,12 +17,24 @@ public class Damage {
 	private MoveSet mset;
 	private Random r;
 	
+	/**
+	 * Default Constructor for the Damage Class.
+	 */
 	Damage(){
 		csd = new CalcSpecialDmg();
 		mset = new MoveSet();
 		r = new Random();
 	}
 	
+	/**
+	 * This method will calculate the damage from an attack sent from a {@code source} monster to a
+	 * {@code target} monster.
+	 * 
+	 * @param atk is the Attack ID.
+	 * @param source is attacking Monster.
+	 * @param target is Monster receiving the attack.
+	 * @return the damage that target should receive.
+	 */
 	public double getDamage(Attack atk, Monster source, Monster target){
 		  Move move = mset.getMove(atk);
 		  double power = (double)move.getPower();
@@ -32,6 +45,66 @@ public class Damage {
 		  
 	}
 	
+	/**
+	 * This method will calculate the highest possible damage from an attack sent from a {@code source} monster to a
+	 * {@code target} monster.
+	 * 
+	 * @param atk is the Attack ID.
+	 * @param source is attacking Monster.
+	 * @param target is Monster receiving the attack.
+	 * @return the damage that target should receive.
+	 */
+	public double highestPossibleDamage(Attack atk, Monster source, Monster target){
+		Move move = mset.getMove(atk);
+		  double power = (double)move.getPower();
+		  double A = offAtt(move, source);
+		  double D = defAtt(move, target);
+		  double modifier = getModifier_noRng(1.0,move, source, target);
+		  return getDmg(power, A/D, modifier);
+	}
+	
+	/**
+	 * This method will calculate the lowest possible damage from an attack sent from a {@code source} monster to a
+	 * {@code target} monster.
+	 * 
+	 * @param atk is the Attack ID.
+	 * @param source is attacking Monster.
+	 * @param target is Monster receiving the attack.
+	 * @return the damage that target should receive.
+	 */
+	public double lowestPossibleDamage(Attack atk, Monster source, Monster target){
+		Move move = mset.getMove(atk);
+		  double power = (double)move.getPower();
+		  double A = offAtt(move, source);
+		  double D = defAtt(move, target);
+		  double modifier = getModifier_noRng(0.85,move, source, target);
+		  return getDmg(power, A/D, modifier);
+	}
+	
+	/**
+	 * Compare all highest possible damage for every attack from source monster's moveset.
+	 * 
+	 * @param atks is a list of the attacking Monster's attacks.
+	 * @param source is attacking Monster.
+	 * @param target is Monster receiving the attack.
+	 * @return the best attack that the attacking monster should use against the target monster.
+	 */
+	public Attack getStrongestMove(List<Attack> atks, Monster source, Monster target){
+		double minimum = Double.MAX_VALUE;
+		Attack best = null;
+		for(Attack atk : atks){
+			double dmg = highestPossibleDamage(atk, source, target);
+			if(dmg < minimum){
+				best = atk;
+				minimum = dmg;
+			}
+		}
+		if(best == null){
+			throw new IllegalArgumentException("[Damage - getStrongestMove]: Best Move not found.");
+		}
+		return best;
+	}
+	
 	private double getDmg(double power, double AD, double modifier){
 		return((((22.0 * power * AD)/50) + 2) * modifier);
 	}
@@ -40,6 +113,15 @@ public class Damage {
 		double multiplier = csd.getMultiplier(move.getElem(), target.getElem());
 		double rng = 0.85 + (0.15) * r.nextDouble();
 		double finalMulti = multiplier * rng;
+		if(move.getElem() == source.getElem()){
+			finalMulti *= 1.5;
+		}
+		return finalMulti;
+	}
+	
+	private double getModifier_noRng(double value, Move move, Monster source, Monster target){
+		double multiplier = csd.getMultiplier(move.getElem(), target.getElem());
+		double finalMulti = multiplier * value;
 		if(move.getElem() == source.getElem()){
 			finalMulti *= 1.5;
 		}
