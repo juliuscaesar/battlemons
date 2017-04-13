aaed6c9ddbae0ff24916c72716cd7a571fb3ff26
Alexandre Dias <dias.al@husky.neu.edu>
2017-04-13 17:34:11 -0400

    Item Changes

--- src/trainers/Item.java
+++ src/trainers/Item.java
@@ -1,17 +1,27 @@
 package trainers;
 
 import monsters.Monster;
+import general.Attack;
 
 public class Item {
 	private final ItemEnum item;
 	private int quantity;
+	private ItemEffect effect;
 	
 	Item(ItemEnum item, int quantity){
 		this.item = item;
 		this.quantity = quantity;
+		this.effect = ItemEffectCreator.getEffect(item);
 	}
 	
-	public void useOn(Monster m){
+	public boolean useOn(Monster m){
+		if(this.effect.canUseItem(m)){
+			this.effect.useItem(m);
+		}
+		return false;
+	}
+	
+	public boolean useOnMove(Monster m, Attack attack){
 		
 	}
 	
--- src/trainers/ItemEffect.java
+++ src/trainers/ItemEffect.java
@@ -9,6 +9,22 @@ public interface ItemEffect {
 	
 	// method for using items that affect the monster (eg: health potion)
 	void useItem(Monster battlemon);
+	
 	// method for using items that affect a monster's moves
 	void useItemOnMove(Monster battlemon, Move move);
+	
+	/**
+	 * Check if the item can be used on the target battlemon.
+	 * @param battlemon is the target battlemon.
+	 * @return true if the item can be used, false if not.
+	 */
+	boolean canUseItem(Monster battlemon);
+	
+	/**
+	 * Check if the item can be used on the target battlemon's target move.
+	 * @param battlemon is the target battlemon.
+	 * @param attack is the target battlemon's target move's enum.
+	 * @return true if the item can be used, false if not.
+	 */
+	boolean canUseItemOnMove(Monster battlemon, Attack attack);
 }
