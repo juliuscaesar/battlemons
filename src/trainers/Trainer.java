a3d05938f6191bcdfe1015cd63a05c8da632b8b0
Alexandre Dias <dias.al@husky.neu.edu>
2017-04-16 15:01:39 -0400

    Trainer Comment

--- src/trainers/Trainer.java
+++ src/trainers/Trainer.java
@@ -40,6 +40,19 @@ public class Trainer {
     public Decision decision; // The decision made by the AI for this player.
     private DT trainerAI = new DT();
 
+    /**
+     * Default Constructor for the Trainer.
+     * It will receive a Name, a List of its Monsters, a List of Items.
+     * The List of Monsters should contain 6 Monsters.
+     * No rules for the List of Items.
+     * 
+     * The List of Monsters will be shuffled, and the first Monster in the shuffled list,
+     * will become the Active Monster.
+     * 
+     * @param name is the Trainer's Name.
+     * @param monsters is the List of Trainer's Monsters.
+     * @param itens is the List of the Trainer's Items.
+     */
     public Trainer(String name, List<Monster> monsters, List<Item> itens) {
         this.name = name;
         this.items = itens;
@@ -57,12 +70,22 @@ public class Trainer {
         active = monsters.get(0);
     }
 
+    /**
+     * This method will clear the List of Monster, and nullify the Active Monster.
+     * After calling this Method, the Trainer can't make any decision. Unless the Trainer is initialized again.
+     */
     public void clearMonsters() {
         this.monsters.clear();
         this.order.clear();
         this.active = null;
     }
 
+    /**
+     * Add more Monsters to the List of Monsters for the Trainer.
+     * If the List of Monster's for Trainer is 6 or Higher, this method wont do anything.
+     * 
+     * @param m is the Monster to be added.
+     */
     public void addMonster(Monster m) {
         if (this.monsters.size() < 6) {
             this.monsters.put(m.getID(), m);
@@ -81,10 +104,16 @@ public class Trainer {
         return list;
     }
 
+    /**
+     * @return the List of Items that this Trainer have.
+     */
     public List<Item> listItems() {
         return items;
     }
 
+    /**
+     * This method will show on the Console, all of this Trainer's Monsters.
+     */
     public void DisplayListOfMonsters() {
         System.out.print(this.name + "\'s team: ");
 
@@ -109,6 +138,12 @@ public class Trainer {
         }
     }
 
+    /**
+     * This method will make the Trainer make a Decision based on the Battle.
+     * 
+     * @param battle is the Battle that the Trainer is currently at.
+     * @return the Decision that the Trainer should make.
+     */
     public Decision makeDecision(Battle battle) {
         decision = trainerAI.makeDecision(battle, this, 0);
         if (decision == null) {
@@ -120,14 +155,24 @@ public class Trainer {
         return decision;
     }
 
+    /**
+     * @return the Active Monster for the Trainer.
+     */
     public Monster getActiveMonster() {
         return active;
     }
 
+    /**
+     * @return the decision this player should make when received the Battle State for the last time.
+     */
     public Decision getDecision() {
         return decision;
     }
 
+    /**
+     * Check if Trainer has any Monsters that are still alive.
+     * @return false if all of the Trainer's Monsters are dead, true if else.
+     */
     public boolean hasAliveMonster() {
         for (Monster m : monsters.values()) {
             if (m.isAlive()) return true;
@@ -135,6 +180,10 @@ public class Trainer {
         return false;
     }
 
+    /**
+     * This method will execute the "Switch Monster" decision.
+     * @param b is the currently Battle the Trainer is in.
+     */
     public void chooseNewMonster(Battle b) {
 
         List<Monster> choices = new ArrayList<Monster>();
@@ -156,6 +205,11 @@ public class Trainer {
                 + ".");
     }
 
+    /**
+     * This method will return the sum of the percentage HP for all monster of this Trainer.
+     * 
+     * @return the sum of the percentage HP for all monster of this Trainer.
+     */
     public float percentOfAllMonsters() {
         float percent = 0;
         for (Monster m : monsters.values()) {
