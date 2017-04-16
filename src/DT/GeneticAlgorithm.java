48052a88c3778433832c081c25b1d4bce6b4fd52
Caesar Nuzzolo <nuzzolo.c@husky.neu.edu>
2017-04-16 12:32:27 -0400

    genetic algorithm methods

--- src/DT/GeneticAlgorithm.java
+++ src/DT/GeneticAlgorithm.java
@@ -2,6 +2,9 @@ package DT;
 
 import trainers.Item;
 import trainers.Trainer;
+
+import java.util.Collections;
+
 import general.Decision;
 import monsters.Monster;
 import moves.Move;
@@ -16,33 +19,33 @@ public class GeneticAlgorithm {
      * Mutate the parameters of a node
      * 
      */
-    public static void mutateParameters(double newParam) {
-    	
+    public static void mutateParameters(DT tree, int index, double newParam) {
+    	if (tree.nodeMap.get(index).condition.uses_parameter) {
+    		tree.nodeMap.get(index).condition.setParam(newParam);
+    	}
     }
 
 	/**
      * Remove a node from the decision tree
      * 
      */
-    public static void removeNode() {
-    	
+    public static void removeNode(DT tree, int index) {
+    	tree.nodeMap.remove(index);
     }
     
     /**
      * Adds a node to the decision tree
      * 
      */
-    public static void addNode() {
-        
-        
+    public static void addNode(DT tree, DecisionNode node) {
+        tree.nodeMap.put(tree.nodeMap.size(), node);
     }
     
     /**
-     * Swaps nodes in a decision tree
+     * Swaps a node in a decision tree with the given node
      * 
      */
-    public static void swapNode() {
-        
-        
+    public static void swapNode(DT tree, int from, DecisionNode newNode) {
+    	tree.nodeMap.replace(from, newNode);
     }
 }
