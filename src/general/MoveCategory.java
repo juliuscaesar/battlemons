dcc23a82042cd890ba85720fcd2839e45aaba8bb
Alexandre Dias <dias.al@husky.neu.edu>
2017-04-17 12:22:23 -0400

    Added MoveCategory Description

--- src/general/MoveCategory.java
+++ src/general/MoveCategory.java
@@ -1,5 +1,17 @@
 package general;
 
+/**
+ * MoveCategory Enum.
+ * 
+ * Each Move must belong to one of the following Categories: { Physical, Special, Status }.
+ * 
+ * Physical Moves are the ones where the Offensive Attribute used on the Damage Calculation are the { Attack }.
+ * 
+ * Special Moves are the ones where the Offensive Attribute used on the Damage Calculation are the { Special Attack }.
+ * 
+ * Status Moves are the ones where the Offensive Attribute used on the Damage Calculation is 0.
+ * But they have 100% chance of Setting a Status on a Monster.
+ */
 public enum MoveCategory{
   Physical, Special, Status,
 }
