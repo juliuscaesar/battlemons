0a940820fade9754aa1de1e50b1422dd5ea78f93
Caesar Nuzzolo <nuzzolo.c@husky.neu.edu>
2017-04-17 10:30:26 -0400

    variables

---
+++ src/general/BattleVariables.java
@@ -0,0 +1,9 @@
+package general;
+
+public class BattleVariables {
+	public static int maxMutationsPerCycle = 500;
+	public static int battlesPerCycle = 50000;
+	public static boolean printEachTurn = false;
+	public static boolean printBattleSummary = true;
+	public static boolean justFitness = true;
+}
